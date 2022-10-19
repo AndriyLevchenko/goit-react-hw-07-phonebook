0f49cc5212657f7b76121f8f27331767e689a485
AndriyLevchenko <98983557+AndriyLevchenko@users.noreply.github.com>
2022-10-19 21:22:06 +0300

    goit-react-hw-03-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,6 +44,20 @@ export class App extends React.Component {
     );
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if(parsedContacts) {
+      this.setState({contacts: parsedContacts});
+    }
+  }
+
   // У формі прописуєм onSubmit як назву пропсу (це не сам сабміт)
   render () {
     const {filter} = this.state;
